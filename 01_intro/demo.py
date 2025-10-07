68912c8db26f8370ef644f2fbaae37c4e4268a34
ravi2krishna <ravi2krishna@gmail.com>
2025-10-07 10:52:57 +0530

    7th Oct - Installations & Syntax Rules

---
+++ 01_intro/demo.py
@@ -0,0 +1,16 @@
+# This is comment, meaning python interpreter will ignore this line
+# function : does a task 
+# we have print() -> printing on the std out 
+
+print(10)
+# print(a) # NameError: name 'a' is not defined
+print("""welcome""") # NameError: name 'welcome' is not defined
+
+today = "tuesday"
+print(today)
+
+day2moro = "wednesday"
+print(day2moro)
+
+# 2moro = "wednesday" # invalid syntax as it starts with digit 
+# print(2moro)
