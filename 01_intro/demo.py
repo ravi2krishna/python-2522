4033466262d2064a6ba1d96f82ee87519e797fbc
ravi2krishna <ravi2krishna@gmail.com>
2025-10-08 11:05:49 +0530

    8th Oct - Variables

--- 01_intro/demo.py
+++ 01_intro/demo.py
@@ -14,3 +14,13 @@ print(day2moro)
 
 # 2moro = "wednesday" # invalid syntax as it starts with digit 
 # print(2moro)
+
+# Improper way of using identifiers 
+x = "Ravi"
+y = 32
+z = "Python"
+
+# Proper way of using identifiers (recommended)
+student_name = "Ravi"
+student_age = 32
+student_course = "Python"
