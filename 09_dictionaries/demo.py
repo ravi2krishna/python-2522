2109115c727b4d6f5ef875d14591fa2b46930bf3
ravi2krishna <ravi2krishna@gmail.com>
2025-10-29 11:09:21 +0530

    29th Oct - Dictionary Methods

--- 09_dictionaries/demo.py
+++ 09_dictionaries/demo.py
@@ -39,3 +39,60 @@ print(num_dict)
 num_dict = {(10):10,2:20,3:30,4:30,5:30} 
 print(type(num_dict))
 print(num_dict)
+
+# real world dictionary use case - JSON Data 
+students = {
+    "101": {
+        "name": "Ravi",
+        "email": "ravi2krishna@gmail.com",
+        "courses": ["python","django","ai"],
+        "course_prices": (10000,20000,30000) 
+    },
+    "102": {
+        "name": "John",
+        "email": "john@yahoo.com",
+        "courses": ["java","spring","ai"],
+        "course_prices": (15000,25000,35000) 
+    }
+}
+
+print(type(students))
+
+print(students)
+
+# Access Data 
+# print(students[0]) # KeyError: 0
+# print(students[101]) # KeyError: 101
+print(students["101"])
+print(students["101"]["name"])
+print(students["101"]["courses"])
+print(students["101"]["courses"][1])
+
+# want to do operations -> check user is google customer
+id = "102"
+if students[id]["email"].endswith("@gmail.com"):
+    print("Google Customer")
+else:
+    print("Not Google Customer")
+    
+# looping the data 
+num_dict = {1:10,2:20,3:30} 
+# print(dir(num_dict))
+print(num_dict)
+
+print(num_dict[2])
+
+# by default we got access to keys
+for num in num_dict:
+    print(num)
+    
+# now access values using keys
+for key in num_dict:
+    print(num_dict[key])
+    
+# using operators to manipulate data
+for key in num_dict:
+    print(num_dict[key] * 10)
+    
+# dict operations
+print(dir(num_dict))
---
+++ 09_dictionaries/ops.py
@@ -0,0 +1,64 @@
+# Dictionary methods 
+
+data = {"a":"apple","b":"banana"} 
+print(data)
+
+# update() : add / update item 
+# if key exists update, if key doesn't exists add
+data.update({"c":"cherry"})
+print(data)
+
+data.update({"a":"apricot"})
+print(data)
+
+# pop() : removes an item by key
+data = {"a":"apple","b":"banana"} 
+# data.pop() # TypeError: pop expected at least 1 argument, got 0
+data.pop("a")
+print(data)
+# data.pop("c") # KeyError: 'c'
+print(data)
+
+# popitem() : removes last item
+data = {"a":"apple","b":"banana"} 
+print(data)
+data.popitem()
+print(data)
+
+# clear() : removes all items, empties dict
+data = {"a":"apple","b":"banana"} 
+print(data)
+data.clear()
+print(data)
+
+# get() : used to get the value for a key, if key doesn't exist returns None
+data = {"a":"apple","b":"banana"} 
+print(data)
+# data.get() # TypeError: get expected at least 1 argument, got 0
+print(data.get("a"))
+print(data.get("c"))
+
+# keys(): used to get keys
+data = {"a":"apple","b":"banana"} 
+print(data)
+print(data.keys())
+keys = data.keys()
+for key in keys:
+    print(key)
+
+# values(): used to get values
+data = {"a":"apple","b":"banana"} 
+print(data)
+print(data.values())
+values = data.values()
+for value in values:
+    print(value)
+
+# items(): gets both keys and values
+data = {"a":"apple","b":"banana"} 
+print(data)
+print(data.items())
+data = data.items()
+for item in data:
+    print(item)
+    print(item[0])
